2a8eb98f995f5fcea05dbf7f2dc654aaa5282927
dragonzilla656 <dragonzilla656@gmail.com>
2023-02-11 12:20:39 -0500

    added comment yay

---
+++ src/main/java/net/dragon/tutorialmod/TutorialModClient.java
@@ -0,0 +1,12 @@
+package net.dragon.tutorialmod;
+
+import net.fabricmc.api.ClientModInitializer;
+
+
+//comment yes
+public class TutorialModClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
